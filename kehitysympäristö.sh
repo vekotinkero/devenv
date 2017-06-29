96bf2cde7ac6f78d532090b59d8863dea0e02416
vekotinkero <veikko.kero@gmail.com>
2017-06-29 09:39:36 +0300

    Add files via upload

---
+++ kehitysympäristö.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Tämä scripti tekee seuraavaa:
+# - asentaa Code::Blocksin
+# - lataa GitHubista Weptechin Contiki forkin ja RIOT-OS:n
+# - päivittää vanhoja repoja
+# - käynnistää ja compilaa Coojan
+# - kääntää ohjelmat Weptechin Sakerille (border router)
+# - tekee upload-launcherit ko. ohjelmille
+#
+# Inx-Service (c) Veikko Kero 2017
+
+echo -e "Follow the instructions on the screen... if any. \n"
+
+# Start in inx-dev-env directory
+cp -r ./upload-scripts/ ../
+
+echo -e "Adding Code::Blocks repository. \n"
+sudo add-apt-repository ppa:damien-moore/codeblocks-stable
+sudo apt-get update
+sudo apt-get -y install codeblocks codeblocks-contrib 
+
+# Move to home folder
+cd ../
+
+echo -e "Great! Next we'll download some git repos... \n"
+git clone git://github.com/Weptech-elektronik/contiki ./contiki-weptech
+git clone git://github.com/JelmerT/cc2538-bsl.git ./contiki-weptech/tools/cc2538-bsl
+git clone git://github.com/RIOT-OS/RIOT.git
+
+echo -e "Done! Next we'll update old repos... \n"
+# Go to basic contiki folder
+cd ./contiki
+git submodule update --init
+
+echo -e "Ok. Now I'll try to compile and start Cooja Network Simualtor. \n"
+# Go to Cooja folder
+cd ./tools/cooja
+sudo ant run
+
+echo -e "Now it's time for some serious compiling... \n"
+
+# Go to Sakers border router example folder
+cd ../../../contiki-weptech/examples/saker/ip64-rpl-border-router
+
+sed -i '94s/.*/#define PLATFORM_CONF_USE_CC1200        0/' project-conf.h
+make TARGET=saker
+mv border-router.bin ../../../../upload-scripts/br-cc1200.bin
+make clean
+
+sed -i '94s/.*/#define PLATFORM_CONF_USE_CC1200        1/' project-conf.h
+make TARGET=saker
+mv border-router.bin ../../../../upload-scripts/br-cc2538.bin
+make clean
+
+# Back to home folder
+cd ../../../../
+cp ./upload-scripts/Saker\ Uploader.desktop ./Desktop
+
+echo -e "Well well well, all done. You're welcome. \n"
+sudo rm -r /home/user/inx-dev-env
